4335eb8785e32ce3cbaf5652293c1d1994c75b32
Devang Agarwal <pbmjaipur@gmail.com>
2022-03-12 10:59:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,16 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyB77S1FiriW2C2LfKf9nULGC26ULqwB8pQ",
+    authDomain: "team-voting-873bd.firebaseapp.com",
+    databaseURL: "https://team-voting-873bd-default-rtdb.firebaseio.com",
+    projectId: "team-voting-873bd",
+    storageBucket: "team-voting-873bd.appspot.com",
+    messagingSenderId: "932389281011",
+    appId: "1:932389281011:web:51ccebac8fa46b59dcab86",
+    measurementId: "G-7SHQTDGDR4"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
